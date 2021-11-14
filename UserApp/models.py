77d0e332a01beaa1ad9c7f50b02b31a05c8fa0cb
Adalberto Pereira <adalbertobosco@gmail.com>
2021-11-14 13:24:12 -0300

    database connect and migrations

---
+++ UserApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    UserId = models.AutoField(primary_key=True)
+    UserName = models.CharField(max_length=100)
+    UserPassword = models.CharField(max_length=32)
+    
+    
--- web_project/settings.py
+++ web_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'UserApp.apps.UserappConfig',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True # TODO change for only have acess the white listed
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,11 +81,15 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'CLIENT': {
+            "host": os.getenv("CONECTIONSTRING"),
+            "name": "prenatal",
+            "authMechanism":"SCRAM-SHA-1"
+            }
     }
 }
-
+#"host": os.getenv("CONECTIONSTRING"),
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
